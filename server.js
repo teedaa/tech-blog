dbd580a21f7daeda4cf84e0b16c302855cd70f11
teedaa <pengtedachengllhs@gmail.com>
2022-11-06 15:03:10 -0800

    Add base files and dependencies

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-const routes = require('./controllers');
+const routes = require('./controllers/api');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 
@@ -13,11 +13,13 @@ const PORT = process.env.PORT || 3001;
 
 // Set up sessions
 const sess = {
-  secret: 'Super secret',
-
+  secret: 'Super secret secret',
+  cookie: {}
   resave: false,
-
-  saveUninitialized: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelizeConnection
+  })
 };
 app.use(session(sess));
 
