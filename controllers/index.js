3e9af07a6dbaf3ee66032404060d6cdfc5609e46
teedaa <pengtedachengllhs@gmail.com>
2022-11-07 15:15:17 -0800

    Set up index files in controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
 
+const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
