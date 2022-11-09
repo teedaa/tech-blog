7087ac13ec067a2a1e1e69a22e7d3a40acc8a23a
teedaa <pengtedachengllhs@gmail.com>
2022-11-09 00:08:22 -0800

    Dashing through the APIs

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-const dashboardRoutes = require('./dashboardRoutes');
+const homeRoutes = require('./home-Routes');
+const dashboardRoutes = require('./dashboard-Routes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
